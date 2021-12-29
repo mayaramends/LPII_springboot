edea4263bd599134e82ba48daea7f3d320b6212e
Mayara Mendes <mayara.mendes.mdc@gmail.com>
2021-12-29 16:11:07 -0300

    bugs arrumados

--- evento/src/main/java/com/example/evento/DataConfiguration.java
+++ evento/src/main/java/com/example/evento/DataConfiguration.java
@@ -1,23 +1,26 @@
 package com.example.evento;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
-import javax.sql.DataSource;
-
 @Configuration
+@Profile("dev")
 public class DataConfiguration {
+
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useTimezone=true&serverTimezone=UTC");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123456");
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:8036/eventosapp?useTimezone=true&serverTimezone=UTC");
+        dataSource.setUsername("mayara");
+        dataSource.setPassword("MdC*123456");
         return dataSource;
     }
 
@@ -27,7 +30,7 @@ public class DataConfiguration {
         adapter.setDatabase(Database.MYSQL);
         adapter.setShowSql(true);
         adapter.setGenerateDdl(true);
-        adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
         adapter.setPrepareConnection(true);
         return adapter;
     }
