488eecdb373d7984605a35a4ca1e60ec2fb2d96b
Mayara Mendes <mayara.mendes.mdc@gmail.com>
2022-01-21 22:20:49 -0300

    melhora do layout das pags, exclusao de elementos do bd

--- evento/src/main/java/com/example/evento/controllers/EventoController.java
+++ evento/src/main/java/com/example/evento/controllers/EventoController.java
@@ -66,6 +66,14 @@ public class EventoController {
         return mv;
     }
 
+    @RequestMapping("/deletarEvento")
+    public String deletarEvento(long codigo) {
+        Evento evento = er.findByCodigo(codigo);
+        er.delete(evento);
+
+        return "redirect:/eventos";
+    }
+
     @RequestMapping(value="/{codigo}", method=RequestMethod.POST)
     public String detalhesEventoPost(@PathVariable("codigo") long codigo, @Valid Convidado convidado, BindingResult result, RedirectAttributes attributes)
     {
@@ -81,5 +89,16 @@ public class EventoController {
         attributes.addFlashAttribute("mensagem", "Convidado adicionado com sucesso!");
 
         return "redirect:/{codigo}";
+    }
+
+    @RequestMapping("/deletarConvidado")
+    public String deletarConvidado(String rg) {
+        Convidado convidado = cr.findByRg(rg);
+        cr.delete(convidado);
+
+        Evento evento = convidado.getEvento();
+        String codigo = "" + evento.getCodigo();
+
+        return "redirect:/" + codigo;
     }
 }
